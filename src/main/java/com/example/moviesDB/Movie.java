be62939c8426b8b53e7bff1cd8432177bee06b1a
Shaolong Li <shaolongli26@gmail.com>
2023-08-19 21:01:38 +0800

    First commit

---
+++ src/main/java/com/example/moviesDB/Movie.java
@@ -0,0 +1,40 @@
+package com.example.moviesDB;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collation = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    @Id
+    private ObjectId id;
+
+    private String imdbId;
+
+    private String title;
+
+    private String releaseDate;
+
+    private String trailerLink;
+
+    private String genres;
+
+    private String poster;
+
+    private List<String> backdrops;
+
+    @DocumentReference
+    private List<Review> reviewIds;
+
+
+}
